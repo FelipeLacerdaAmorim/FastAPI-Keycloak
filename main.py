48d20131af3a356fb8b681d57a4fe197feee8476
Felipe Laceda <joaofelipeamorim2001@gmail.com>
2023-10-20 09:56:32 -0300

    All endpoints working

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import uvicorn
 from starlette import status
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.responses import RedirectResponse
-from fastapi_keycloak import FastAPIKeycloak, OIDCUser, UsernamePassword
-from pydantic import BaseModel
+from fastapi_keycloak import FastAPIKeycloak, OIDCUser, UsernamePassword, KeycloakUser
+from pydantic import BaseModel, SecretStr
 
 app = FastAPI()
 
@@ -27,55 +27,100 @@ ALGORITHM = 'RS256'
 oauth2_bearer = OAuth2PasswordBearer(tokenUrl='/token',
                                      scheme_name="direct_oauth2_schema")
 
-
-# @app.get("/user")  # Requires logged in
-# def current_users(user: OIDCUser = Depends(idp.get_current_user())):
-#     return user.name
-
-
-@app.get("/user-test")  # Requires logged in
-async def get_current_user(token:Annotated[str, Depends(oauth2_bearer)]):
-#async def get_current_user(token:str):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get('name')
-        if username is None or username is None:
-             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                                 detail='Could not validate user.')
-        return {'username': username}
-    except JWTError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail='Could not validate user.')
+# Classes auxiliares
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
 
 
-@app.post("/token")
+# Endpoints
+@app.post("/token", tags=["auth-flow"])
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     token_aux = idp.user_login(username=form_data.username, password=form_data.password)
     token = token_aux.access_token
     return {'access_token': token, 'token_type': 'bearer'}
 
-@app.get("/admin")  # Requires the admin role
+
+@app.get("/admin", tags=["auth-flow"])  # Requires the admin role
 def company_admin(user: OIDCUser = Depends(idp.get_current_user(required_roles=["admin"]))):
     return f'Hi admin {user}'
 
 
-# @app.get("/login")
-# def login_redirect():
-#     return RedirectResponse(idp.login_uri)
-
-# @app.get("/auto-login")
-# def login(user: UsernamePassword = Depends()):
-#     return idp.user_login(username=user.username, password=user.password.get_secret_value())
-
-
-@app.get("/callback")
+@app.get("/callback", tags=["auth-flow"])
 def callback(session_state: str, code: str):
     return idp.exchange_authorization_code(session_state=session_state, code=code)  # This will return an access token
 
-# @app.get("/auto-callback", response_model=Callback)
-# def callback():
-#     await 
-#     return idp.exchange_authorization_code(session_state=session_state, code=code)  # This will return an access token
+
+@app.get("/logout", tags=["auth-flow"])
+def logout():
+    return idp.logout_uri
+
+
+# Menagement Endpoints
+@app.get("/user", tags=["user-management"])  # Requires logged in
+async def get_current_user(token:Annotated[str, Depends(oauth2_bearer)]):
+#async def get_current_user(token:str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('name')
+        if username is None:
+             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                 detail='Could not validate user.')
+        return{'username': username}
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='Could not validate user.')
+    
+
+@app.post("/users", tags=["user-management"])
+async def create_user(model: CreateUserRequest):   
+    idp.create_user(model.first_name, model.last_name, model.username, model.email, model.password, True, None, False, None)
+    try:
+        user = idp.get_user(query=f'username={model.username}')
+        return user
+    except:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail='Could not validate user.')
+
+@app.put("/user", tags=["user-management"])
+def update_user(first_name:str, last_name:str, email:str,#user: KeycloakUser,
+                token:Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('preferred_username')
+
+        if username is None:
+             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                 detail='Could not validate user.')
+        
+        #user_id: str = payload.get('sub')
+        user_aux = idp.get_user(query=f'username={username}')
+        user_aux.firstName = first_name
+        user_aux.lastName = last_name
+        user_aux.email = email
+
+        return idp.update_user(user=user_aux)
+        
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='Could not validate user.')
+    
+
+
+@app.put("/user/{user_id}/change-password", tags=["user-management"])
+def change_password(user_id: str, new_password: SecretStr,
+                    token:Annotated[str, Depends(oauth2_bearer)]):
+    return idp.change_password(user_id=user_id, new_password=new_password)
+
+
+@app.put("/user/{user_id}/send-email-verification", tags=["user-management"])
+def send_email_verification(user_id: str,
+                            token:Annotated[str, Depends(oauth2_bearer)]):
+    return idp.send_email_verification(user_id=user_id)
+
 
 
 if __name__ == '__main__':
