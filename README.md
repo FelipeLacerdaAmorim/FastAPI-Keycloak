c4b15afa5927e5b8a42c93231efad5f0fc1c708f
Felipe Lacerda <joaofelipeamorim2001@gmail.com>
2024-03-26 14:58:20 -0300

    Update

--- README.md
+++ README.md
@@ -1,15 +1,40 @@
 # FastAPI-Keycloak
-## Bibliotecas
 
-pip install fastapi_keycloak<br/>
-pip install pydantic==1.10.9<br/>
-pip install PyJWT<br/>
-pip install python_jose<br/>
+## Installing Libraries
+
+```
+pip install fastapi_keycloak
+```
+
+```
+pip install pydantic==1.10.9
+```
+
+```
+pip install PyJWT
+```
+
+```
+pip install python_jose
+```
+
+```
 pip install python-multipart
+```
+
+```
+pip install cryptography
+```
+
 
 ## Introdução
 
-Primeiramente, execute o 'docker-compose up -d'<br/>
+Primeiramente, execute o 
+
+```
+docker-compose up -d
+```
+
 Isso irá permitir o acesso ao keycloak na url definida: localhost:8085/auth<br/>
 O usuário e senha de administrador também estão definidos do docker-compose: keycloakuser, keycloakpassword<br/>
 Em seguida, configure o Keycloak com base no tutorial em: 'https://fastapi-keycloak.code-specialist.com/keycloak_configuration/'<br/>
@@ -19,15 +44,30 @@ Os nomes que estão por padrão são: Realm = Test, Client = test-client. Caso c
 
 A configuração da API consiste em passar os devidos valores do parâmetro idp.
 
-idp = FastAPIKeycloak(
-        server_url="http://localhost:8085/auth",<br/>
-        client_id="test-client",<br/>
-        client_secret="Client Secret na seção Credentials do cliente que foi criado",<br/>
-        admin_client_secret="Admin Client Secret na seção Credentials do cliente admin-cli",<br/>
-        realm="Test",<br/>
-        callback_uri="http://localhost:8081/callback"<br/>
-)
 
-Alem disso, deve-se copiar a public key do algoritmo RS256 contida em Realm Settings > Keys<br/>
-SECRET_KEY = b'-----BEGIN PUBLIC KEY-----\n{Insira a chave aqui}\n-----END PUBLIC KEY-----'<br/>
-Após isso, basta executar a API: uvicorn main:app<br/>
+```
+idp = FastAPIKeycloak(
+        server_url="http://localhost:8085/auth",
+        client_id="Nome do Client criado",
+        client_secret="Client Secret na seção Credentials do cliente que foi criado",
+        admin_client_secret="Admin Client Secret na seção Credentials do cliente admin-cli",
+        realm="Nome do Realm",
+        callback_uri="Url de callback"
+)
+```
+
+Alem disso, deve-se copiar a public key do algoritmo RS256 contida em Realm Settings > Keys
+
+```
+SECRET_KEY = b'-----BEGIN PUBLIC KEY-----\n{Insira a chave aqui}\n-----END PUBLIC KEY-----'
+```
+
+Após isso, basta executar a API: 
+
+```
+uvicorn main:app
+```
+
+A documentação do Swagger está em:
+
+http://localhost:8000/docs
--- main.py
+++ main.py
@@ -2,27 +2,30 @@ from typing import Annotated
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError
 import jwt
+import requests
 import uvicorn
 from starlette import status
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.responses import RedirectResponse
 from fastapi_keycloak import FastAPIKeycloak, OIDCUser, UsernamePassword, KeycloakUser
 from pydantic import BaseModel, SecretStr
+import urllib
+
 
 app = FastAPI()
 
 idp = FastAPIKeycloak(
-    server_url="http://localhost:8085/auth",
+    server_url="http://localhost:8085/",
     client_id="test-client",
-    client_secret="NqGPUG9ynh01zRBENauRjAJFA6O8rN3X",
-    admin_client_secret="1qmHrTKnChfxSvc4WpMnAcLjISWmFrIo",
+    client_secret="R1dkfXz8PGNSQYiBGDE0MxlXPXsaeoo2",
+    admin_client_secret="Rb4pyxhCpOFsgpS1AldkF8r1axL9eem1",
     realm="Test",
     callback_uri="http://localhost:8081/callback"
 )
 idp.add_swagger_config(app)
 
-SECRET_KEY = b'-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzInP4hb/1ZtInNs0zKv7qT4PvNnyoFhmrF1sRfwZd6BrrnFjdrgHRbGsDxOuTH0OCgbmety4o6ertKOuw0Z6/QaAjE8jdNUCq5XDK+U374UFKgasvMl+PNk9KeaRBpQLWqwLo7HtHmEnSe2kfMWdGbEkfd//WKB9LiXCVql9qFp2CRweRfkZ85jDsJ0TaSS/dkC+Kthfjdp6NpnlIeno0PmZbe+FObmz2to2sLcp22y//k9bOPMN5cBBCaPNxIWFv6P+GoGALcFeyoUVMVnLK0bu5Z/2fP2+lr1vlcwibI8epIhvMFqxCo3Sy2IhKLrgGH1iA1Nubc2WkDBu6ftJ2wIDAQAB\n-----END PUBLIC KEY-----'
-ALGORITHM = 'RS256'                        
+SECRET_KEY = b'-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0Eq3ytZLnXoje1NPZW5Cwjh6Y8vnvV8Byo2n0T0RNQvbcEc5NmfJsxYuh5mnCGxlbiucB4zJhFZAdg+1nDpCV1IgUmbBXQmj8cea1qd8Rj8PENAXE52GM5TuXtAMD+U4rKgZj8TQq7XSCp43cYV108B/TXRaaYlVGfNAVNmSscOVlDgq177/4t53nbJhkjRNMzCpZ4LgkMpzDZgl0Vk8bYwRx+wt6XKtrpU2ho5IK3/SxA3ubdTz5GahCNuoxRo90f92EQGHM/v4nKmxsdyhEbKHFFtYx8aKr6SIVOFReUcbgOmT1QN1+qTteA4c1os5gumEVP+pu9MracobYdE4oQIDAQAB\n-----END PUBLIC KEY-----'
+ALGORITHM = 'RS256'                  
 
 oauth2_bearer = OAuth2PasswordBearer(tokenUrl='/token',
                                      scheme_name="direct_oauth2_schema")
@@ -40,8 +43,9 @@ class CreateUserRequest(BaseModel):
 @app.post("/token", tags=["auth-flow"])
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     token_aux = idp.user_login(username=form_data.username, password=form_data.password)
-    token = token_aux.access_token
-    return {'access_token': token, 'token_type': 'bearer'}
+    #token = token_aux.access_token
+    #return {'access_token': token, 'token_type': 'bearer'}
+    return token_aux
 
 
 @app.get("/admin", tags=["auth-flow"])  # Requires the admin role
@@ -54,9 +58,33 @@ def callback(session_state: str, code: str):
     return idp.exchange_authorization_code(session_state=session_state, code=code)  # This will return an access token
 
 
-@app.get("/logout", tags=["auth-flow"])
-def logout():
-    return idp.logout_uri
+@app.post("/logout", tags=["auth-flow"])
+def logout(access_token, refresh_token):
+    try:
+        url = 'http://localhost:8085/realms/Test/protocol/openid-connect/logout'
+
+        payload = jwt.decode(access_token, SECRET_KEY, audience="account", algorithms=[ALGORITHM])
+        username: str = payload.get('name')
+        if username is None:
+             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                 detail='Could not validate user.')
+        
+        user_id = payload.get('id')
+
+        data = {'client_id': user_id, 'refresh_token': refresh_token}
+
+        data = urllib.parse.urlencode(data)
+
+        resp = requests.get(url=url, params=data, headers=headers)
+
+        response = requests.post()
+
+        return response
+    
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='Could not validate user.')
+    #return idp.logout_uri
 
 
 # Menagement Endpoints
